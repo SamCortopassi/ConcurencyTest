952ce55baae5a1a6eecbe5f2069892bae4272893
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-03-27 13:12:10 -0500

    test

--- ConcurencyTest/ViewController.swift
+++ ConcurencyTest/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func calculatePrimeNumber(_ sender: Any) {
+        for number in 0 ... 100_000_000 {
+            let isPrimeNumber = isPrime(number: number)
+            print("\(number) is prime: \(isPrimeNumber)")
+        }
+    }
+    
+    @IBOutlet weak var primeNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func isPrime(number: Int) -> Bool {
+        if number <= 1 {
+            return false
+        }
+        if number <= 3 {
+            return true
+        }
+        var i = 2
+        while i * i <= number {
+            if number % i == 0 {
+                return false
+            }
+            i = i + 2
+        }
+    return true
+    }
 
 }
 
